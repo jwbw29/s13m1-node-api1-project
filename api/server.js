915b2ee47c86eea9cc1de9f55ede32258e22cd12
Justin Byrd <justinbyrd7@gmail.com>
2023-11-14 15:18:41 -0600

    server mildly running

--- api/server.js
+++ api/server.js
@@ -1,7 +1,12 @@
 // BUILD YOUR SERVER HERE
 const express = require("express");
-
 const server = express();
-server.use(express.json());
 
-module.exports = { server }; // EXPORT YOUR SERVER instead of {}
+const users = require("./users/model");
+console.log("HERE'S THE DATA for USERS: ", users);
+
+server.use("*", (req, res) => {
+  res.status(404).json({ message: "not found" });
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const users = require("./api/users/model");
-console.log("HERE'S THE DATA for USERS: ", users);
-
 const server = require("./api/server");
 
 const port = 8000;
 
 // START YOUR SERVER HERE
+// server.get("/", (res, req) => {
+//   // landing page
+//   res.json({
+//     message:
+//       "Welcome to the landing page of this server.\nTo see more you'll need to append '/users' or '/users{id}' to the end of the URL.",
+//   });
+// });
+
+// [ ] get ALL users
+// server.get()
+
+// [ ] get user by ID
+// server.get()
+
+// [ ] create a new user
+// server.post()
+
+// [ ] update a user
+// server.put()
+
+// [ ] delete a user
+// server.delete()
+
+server.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+})
