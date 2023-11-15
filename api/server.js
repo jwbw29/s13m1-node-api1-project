d8494fd64c4f7608afdd23dd0e8a4bcfd34d166e
Justin Byrd <justinbyrd7@gmail.com>
2023-11-15 08:54:50 -0600

    added error path for delete

--- api/server.js
+++ api/server.js
@@ -69,15 +69,23 @@ server.post("/api/users", (req, res) => {
 
 // [x] delete a user
 server.delete("/api/users/:id", async (req, res) => {
-  const possibleUser = await User.findById(req.params.id);
+  try {
+    const possibleUser = await User.findById(req.params.id);
 
-  if (!possibleUser) {
-    res.status(404).json({
-      message: "The user with the specified ID does not exist",
+    if (!possibleUser) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist",
+      });
+    } else {
+      const deletedUser = await User.remove(possibleUser.id);
+      res.status(200).json(deletedUser);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Error Deleting User",
+      err: err.message,
+      stack: err.stack,
     });
-  } else {
-    const deletedUser = await User.remove(possibleUser.id);
-    res.status(200).json(deletedUser);
   }
 });
 
